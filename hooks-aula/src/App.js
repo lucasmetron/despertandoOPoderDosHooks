f0e9e2607d75846b7d9a77ead35f4d198330b753
lucasmetron <lucasmetron@gmail.com>
2021-08-18 17:28:06 -0300

    comparando com classe

--- hooks-aula/src/App.js
+++ hooks-aula/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
+function Counter(props) {
+  const [valor, setValor] = useState(0);
+
+  return <button onClick={() => setValor(valor + 1)}> {`Valor: ${valor}`}</button >
+}
+
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Counter />
   );
 }
 
