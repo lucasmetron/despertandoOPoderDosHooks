085848f53f0116f8af2f2dac6e7f7aa690df37cb
lucasmetron <lucasmetron@gmail.com>
2021-08-19 17:04:24 -0300

    ref

--- hooks-aula/src/App.js
+++ hooks-aula/src/App.js
@@ -3,46 +3,28 @@ import './App.css';
 
 function Counter(props) {
   const [valor, setValor] = useState(0);
+  const valorRef = useRef(0);
   const [title, setTitle] = useState('');
-  const defaultRef = createRef();
-  const hookRef = useRef();
+
 
   useEffect(() => {
     setInterval(() => {
-      setValor(v => v + 1)
       console.log(valor)
+      console.log(valorRef)
     }, 1000)
 
   }, [])
 
   useEffect(() => {
-    console.log(valor)
+
+    valorRef.current = valor
+
   }, [valor])
-
-
-  // useEffect(() => {
-  //   setInterval(() => {
-  //     setValor(valor + 1)
-
-  //   }, 1000)
-
-  // }, [])
-
-  // useEffect(() => {
-  //   console.log(valor)
-  // }, [valor])
-
-
 
   return (
     <>
-
       <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
       <button onClick={() => setValor(valor + 1)}> {`Valor: ${valor}`}</button >
-
-      <div ref={defaultRef}></div>
-      <div ref={hookRef}></div>
-
     </>
   );
 
