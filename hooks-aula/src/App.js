b040a88e208573222041687753a45b85147f2346
lucasmetron <lucasmetron@gmail.com>
2021-08-19 16:39:58 -0300

    useRef()

--- hooks-aula/src/App.js
+++ hooks-aula/src/App.js
@@ -1,27 +1,34 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, createRef } from 'react'
 import './App.css';
 
 function Counter(props) {
   const [valor, setValor] = useState(0);
   const [title, setTitle] = useState('');
-
+  const defaultRef = createRef();
+  const hookRef = useRef();
 
 
   useEffect(() => {
-    console.log(3)
-    document.title = title
-  }, [title])
+    console.log('EFFECT')
+    console.log('default', defaultRef)
+    console.log('hook', hookRef)
 
+  }, [])
 
+  console.log('default', defaultRef)
+  console.log('hook', hookRef)
 
   return (
     <>
+
       <input type="text" value={title} onChange={(event) => setTitle(event.target.value)} />
       <button onClick={() => setValor(valor + 1)}> {`Valor: ${valor}`}</button >
+
+      <div ref={defaultRef}></div>
+      <div ref={hookRef}></div>
+
     </>
   );
-
-
 
 }
 
