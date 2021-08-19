a660d7fc6cb0f37308cd0c1c45bd59cd8f8a4f27
lucasmetron <lucasmetron@gmail.com>
2021-08-19 15:10:20 -0300

    useEffect

--- hooks-aula/src/App.js
+++ hooks-aula/src/App.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, createRef } from 'react'
 import './App.css';
 
 function Counter(props) {
   const [valor, setValor] = useState(0);
-  return <button onClick={() => setValor(valor + 1)}> {`Valor: ${valor}`}</button >
+  const button = createRef();
+
+  console.log(1, button)
+  useEffect(() => {
+    console.log(3, button)
+  })
+  console.log(2, button)
+
+
+  return <button ref={button} onClick={() => setValor(valor + 1)}> {`Valor: ${valor}`}</button >
 }
 
 function App() {
