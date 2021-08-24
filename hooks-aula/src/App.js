3458e8fdd5e61005226998ef6d2999e6b03dbf4d
lucasmetron <lucasmetron@gmail.com>
2021-08-24 17:19:05 -0300

    useWindowSize

--- hooks-aula/src/App.js
+++ hooks-aula/src/App.js
@@ -1,15 +1,14 @@
 import React, { useEffect, useRef, useState } from 'react'
-import useKeyPress from './data/hooks/useKeyPress';
+import useWindowSize from './data/hooks/useWindowSize';
 
 function App() {
-
-  const keyA = useKeyPress('a')
-  const keyB = useKeyPress('b')
+  const windowSize = useWindowSize();
 
   return (
     <div>
-      {keyA && "A"} {/* se keyA for true ele mostra 'A' */}
-      {keyB && "B"}
+      Largura : {windowSize.width}
+      <br />
+      Altura : {windowSize.heigth}
     </div>
   );
 
