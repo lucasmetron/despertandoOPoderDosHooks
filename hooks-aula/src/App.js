4958ddb1d80f0c0da605e8d232422f3f73cafc0b
lucasmetron <lucasmetron@gmail.com>
2021-08-24 15:49:12 -0300

    hook localStorage

--- hooks-aula/src/App.js
+++ hooks-aula/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useOnlineStatus } from './data/hooks/useOnlineStatus';
+import useLocalStorage from './data/hooks/useLocalStorage';
 
 function App() {
   const isOnline = useOnlineStatus();
+  const [counter, setCounter] = useLocalStorage('counter', 1);
 
   return (
     <div>
       {isOnline ? 'conectado' : 'desconectado'}
+
+      <button onClick={() => setCounter(counter + 1)}>{counter}</button>
     </div>
   );
 }
